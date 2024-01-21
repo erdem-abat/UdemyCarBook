399bda1cef0b958c6a7ba9deaeb7d8baf6af9de5
erdem abat <erdemabat@hotmail.com>
2024-01-21 10:41:32 +0300

    app-domain-api-persistence added

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Commands.AboutCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class UpdateAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+        public UpdateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateAboutCommand command)
+        {
+            var data = await _repository.GetByIdAsync(command.AboutID);
+            data.Title = command.Title;
+            data.Description = command.Description;
+            data.ImageUrl = command.ImageUrl;
+            await _repository.UpdateAsync(data);
+        }
+    }
+}

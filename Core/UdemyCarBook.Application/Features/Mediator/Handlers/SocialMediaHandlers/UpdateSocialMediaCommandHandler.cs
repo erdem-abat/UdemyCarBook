4295804c2a7fa5b955503f50fe667cc9cbc29f14
Mert Dere <Kangrendayi@yandex.com>
2024-01-21 22:41:41 +0300

    Services & SocialMedia CQRS added by using MediaTR

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/SocialMediaHandlers/UpdateSocialMediaCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using UdemyCarBook.Application.Features.Mediator.Commands.SocialMediaCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.SocialMediaHandlers
+{
+    public class UpdateSocialMediaCommandHandler : IRequestHandler<UpdateSocialMediaCommand>
+    {
+        private readonly IRepository<SocialMedia> _repository;
+
+        public UpdateSocialMediaCommandHandler(IRepository<SocialMedia> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateSocialMediaCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.SocialMediaID);
+            value.Url = request.Url;
+            value.Icon = request.Icon;
+            value.Name = request.Name;
+            await _repository.UpdateAsync(value);
+        }
+    }
+}

37595aa08ad69dc2d6c022acaffadfeded15f945
Mert Dere <Kangrendayi@yandex.com>
2024-01-22 22:13:42 +0300

    Defaul Page edited

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/CarHandlers/GetLast5CarsWithBrandQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Results.CarResults;
+using UdemyCarBook.Application.Interfaces.CarInterfaces;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.CarHandlers
+{
+    public class GetLast5CarsWithBrandQueryHandler
+    {
+        private readonly ICarRepository _carrepository;
+
+        public GetLast5CarsWithBrandQueryHandler(ICarRepository carrepository)
+        {
+            _carrepository = carrepository;
+        }
+
+        public List<GetLast5CarsWithBrandQueryResult> Handle()
+        {
+            var values = _carrepository.GetLast5CarsWithBrands();
+            return values.Select(x => new GetLast5CarsWithBrandQueryResult
+			{
+                CarId = x.CarId,
+                BrandName = x.Brand.Name,
+                BrandID = x.BrandID,
+                BigImageUrl = x.BigImageUrl,
+                CoverImageUrl = x.CoverImageUrl,
+                Fuel = x.Fuel,
+                Km = x.Km,
+                Luggage = x.Luggage,
+                Model = x.Model,
+                Seat = x.Seat,
+                Transmission = x.Transmission
+            }).ToList();
+        }
+    }
+}
